74f0ca49389905cb1d271f4a0f14960b3ea0120f
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2024-07-10 21:02:46 +0300

    compile updated

--- migrations/compile.ts
+++ migrations/compile.ts
@@ -27,6 +27,13 @@ const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
 const compiledContract = output.contracts["Slot.sol"].Slot;
 
+fs.mkdirSync(path.resolve(__dirname, "..", "build"), { recursive: true });
+
+fs.writeFileSync(
+	path.resolve(__dirname, "..", "build", "Slot.json"),
+	JSON.stringify(compiledContract, null, 2)
+);
+
 if (!compiledContract) {
 	throw new Error("Contract compilation failed");
 }
